95c5c16130b404a97f897cd7fb2fc809bea589bf
Pradnyads <sorepradnya@gmail.com>
2020-11-10 17:11:20 +0530

    Added : Choose Symbol for user and computer Game

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -4,10 +4,21 @@ echo " "
 echo "TicTacToe Game"
 declare -a sq
 sq=(1 2 3 4 5 6 7 8 9)
-if [[((RANDOM%2 = 1))]];then
+
+#UC2 Assign a Symbol to user and computer
+ read n
+ if [[ $n == 1 ]]
+ then
   echo "Entry for user x"
   echo "Entry for computer is o"
-else
+ elif [[ $n == 2 ]]
+ then
   echo "Entry for user o"
   echo "Entry for computer is x"
+ else
+  echo "Wrong input"
 fi
+
+
+
+
